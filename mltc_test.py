117f34b6cdd689ebfad2df3cd26b95948bb7337e
Jiri Tomicek <j.tomicek@gmail.com>
2023-10-26 17:22:20 +0200

    add mask determination to mltc prototype

--- mltc_test.py
+++ mltc_test.py
@@ -4,7 +4,7 @@ import ogr
 import osr
 import os
 
-site_bbox_filepath = "/media/jiri/ImageArchive/GW_MLTC_TEST/EUGW_TestSites/CZ0314123_BBox.shp"
+site_bbox_filepath = "/media/jtomicek/ImageArchive/GW_MLTC_TEST/EUGW_TestSites/CZ0314123_BBox.shp"
 # site_bbox_filepath = "/media/jiri/ImageArchive/GW_MLTC_TEST/EUGW_TestSites/ES6110005_BBox.shp"
 
 start_date = "2020-03-01"
@@ -35,6 +35,10 @@ def get_aoi_bounds_wgs(file_path):
             lon.append(point[1])
     return {"lat_max": max(lat), "lat_min": min(lat), "lon_max": max(lon), "lon_min": min(lon)}
 
+def max_ndvi_selection(ndvi):
+    max_ndvi = ndvi.max()
+    return ndvi.array_apply(lambda x:x!=max_ndvi)
+
 
 # establish connection to OpenEO backend (reuse existing authentication)
 connection = openeo.connect("openeo.cloud").authenticate_oidc()
@@ -42,10 +46,63 @@ connection = openeo.connect("openeo.cloud").authenticate_oidc()
 # get site bounds in wgs
 aoi_bounds = get_aoi_bounds_wgs(site_bbox_filepath)
 
-# get source satellite images as datacube
-datacube = connection.load_collection(
+# set temporal extent
+temporal_extent = [start_date, end_date]
+
+# create binary cloud masks
+scl = connection.load_collection(
     "SENTINEL2_L2A",
-    spatial_extent={"west": aoi_bounds["lon_min"], "south": aoi_bounds["lat_min"], "east": aoi_bounds["lon_max"], "north": aoi_bounds["lat_max"]},
-    temporal_extent=["2020-03-01", "2020-03-31"],
-    bands=bands_required)
-print(datacube)
+    spatial_extent={"west": aoi_bounds["lon_min"], "south": aoi_bounds["lat_min"], "east": aoi_bounds["lon_max"],
+                    "north": aoi_bounds["lat_max"]},
+    temporal_extent=temporal_extent,
+    bands = ["SCL"],
+    max_cloud_cover=95
+)
+
+cloud_mask = scl.process(
+    "to_scl_dilation_mask",
+    data=scl,
+    kernel1_size=17, kernel2_size=77,
+    mask1_values=[2, 4, 5, 6, 7],
+    mask2_values=[3, 8, 9, 10, 11],
+    erosion_kernel_size=3)
+
+# load RED and NIR bands
+ndvi_bands = connection.load_collection(
+    "SENTINEL2_L2A",
+    temporal_extent = temporal_extent,
+    spatial_extent={"west": aoi_bounds["lon_min"], "south": aoi_bounds["lat_min"], "east": aoi_bounds["lon_max"],
+                    "north": aoi_bounds["lat_max"]},
+    bands = ["B04", "B08"],
+    max_cloud_cover=95
+)
+
+# mask them with cloud mask
+ndvi_bands = ndvi_bands.mask(cloud_mask)
+
+# calculate NDVI
+ndvi = ndvi_bands.ndvi(nir="B08",red="B04")
+
+rank_mask = ndvi.apply_neighborhood(
+        max_ndvi_selection,
+        size=[{'dimension': 'x', 'unit': 'px', 'value': 1}, {'dimension': 'y', 'unit': 'px', 'value': 1},
+              {'dimension': 't', 'value': "month"}],
+        overlap=[]
+    )
+
+print(rank_mask)
+
+rank_mask.filter_bbox({"west": aoi_bounds["lon_min"], "south": aoi_bounds["lat_min"], "east": aoi_bounds["lon_max"],
+                    "north": aoi_bounds["lat_max"]}).execute_batch("/media/jtomicek/ImageArchive/GW_MLTC_TEST/mask.nc")
+
+# rank_mask.download_file("/media/jtomicek/ImageArchive/GW_MLTC_TEST/mask.tif")
+
+
+
+# # get source satellite images as datacube
+# datacube = connection.load_collection(
+#     "SENTINEL2_L2A",
+#     spatial_extent={"west": aoi_bounds["lon_min"], "south": aoi_bounds["lat_min"], "east": aoi_bounds["lon_max"], "north": aoi_bounds["lat_max"]},
+#     temporal_extent=["2020-03-01", "2020-03-31"],
+#     bands=bands_required)
+# print(datacube)
